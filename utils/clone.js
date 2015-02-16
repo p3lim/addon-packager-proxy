b272c170599c2079dde1bd0fdab71f7106dbe51c
Adrian L Lange <mail@p3lim.net>
2015-02-16 18:07:10 +0100

    Grab the changelog from the git repository
    This allows not needing to package the changelog with the zip in the packager.

---
+++ utils/clone.js
@@ -0,0 +1,18 @@
+var Strings = module.exports.Strings;
+
+var spawn = require('child_process').spawn;
+module.exports = function(repo, name, callback){
+	var path = './tmp/' + name;
+
+	var args = ['clone', '--'];
+	args.push(repo);
+	args.push(path);
+
+	var process = spawn('git', args);
+	process.on('close', function(status){
+		if(status == 0)
+			callback(null, path);
+		else
+			callback(new Error(Strings.CLONE_ERROR.replace('%s', status)));
+	});
+}
