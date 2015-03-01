c044a6033e4856a5290445181ad51fb164462eac
Adrian L Lange <mail@p3lim.net>
2015-03-01 01:15:02 +0100

    Send the compatible game version to WowInterface

--- files.js
+++ files.js
@@ -4,6 +4,7 @@ var fs = require('fs'),
 var Utils = require('./utils');
 
 var repo = 'git://git.curseforge.net/wow/%s/mainline.git';
+
 module.exports.fetchChangelog = function(details, callback){
 	Utils.Clone(repo.replace('%s', details.curse), details.curse, function(err, path){
 		if(err)
@@ -26,4 +27,29 @@ module.exports.fetchChangelog = function(details, callback){
 
 module.exports.formatChangelog = function(data){
 	return Utils.HTMLToBBCode(markdown.toHTML(data));
+}
+
+module.exports.getInterfaceVersion = function(details, callback){
+	Utils.Clone(repo.replace('%s', details.curse), details.curse, function(err, path){
+		if(err)
+			callback(err);
+		else {
+			var filePath = path + '/' + details.path + '.toc';
+			if(!fs.existsSync(filePath))
+				callback(new Error(Utils.Strings.TOC_MISSING))
+			else {
+				fs.readFile(filePath, {encoding: details.encoding || 'utf8'}, function(err, data){
+					if(err)
+						callback(err);
+					else {
+						var interfaceVersion = data.match(/Interface: ?(.+)/);
+						if(!interfaceVersion)
+							callback(new Error(Utils.Strings.INTERFACE_VERSION_MISSING));
+						else
+							callback(interfaceVersion[1]);
+					}
+				});
+			}
+		}
+	});
 }
