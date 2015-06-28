d24eb85bab43d052ec62e341532d087a5b3f6105
Adrian L Lange <mail@p3lim.net>
2015-06-28 16:06:52 +0200

    Merge into existing local repositories if they exist
    Should resolve issues when trying to upload before server idles and clears the cached repositories

--- utils/clone.js
+++ utils/clone.js
@@ -1,36 +1,31 @@
 var Strings = require('./strings');
 
 var fs = require('fs'),
-	spawn = require('child_process').spawn;
-
-var deleteDir = function(path){
-	if(fs.existsSync(path)){
-		fs.readdirSync(path).forEach(function(file, index){
-			var curPath = path + '/' + file;
-			if(fs.lstatSync(curPath).isDirectory()){
-				deleteDir(curPath);
-			} else {
-				fs.unlinkSync(curPath);
-			}
-		});
-
-		fs.rmdirSync(path);
-	}
-}
+	cp = require('child_process');
 
 module.exports = function(repo, name, callback){
 	var path = './tmp/' + name;
-	deleteDir(path);
+	if(fs.existsSync(path)){
+		var fetch = cp.spawnSync('git', ['fetch', 'origin', 'master']);
+		if(fetch.status == 0){
+			var merge = cp.spawnSync('git', ['merge', 'origin/master']);
+			if(merge.status == 0)
+				callback(null, path);
+			else
+				callback(new Error(Strings.MERGE_ERROR.replace('%s', merge.status)));
+		} else
+			callback(new Error(Strings.FETCH_ERROR.replace('%s', fetch.status)));
+	} else {
+		var args = ['clone', '--'];
+		args.push(repo);
+		args.push(path);
 
-	var args = ['clone', '--'];
-	args.push(repo);
-	args.push(path);
-
-	var process = spawn('git', args);
-	process.on('close', function(status){
-		if(status == 0)
-			callback(null, path);
-		else
-			callback(new Error(Strings.CLONE_ERROR.replace('%s', status)));
-	});
+		var process = cp.spawn('git', args);
+		process.on('close', function(status){
+			if(status == 0)
+				callback(null, path);
+			else
+				callback(new Error(Strings.CLONE_ERROR.replace('%s', status)));
+		});
+	}
 }
